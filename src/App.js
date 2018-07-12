1d56eccdda0e65b1befbcce6183fab0c74ad6f0f
Tyler Weeres <tweeres04@gmail.com>
2018-07-11 21:59:40 -0700

    Form works

--- src/App.js
+++ src/App.js
@@ -1,28 +1,100 @@
 import React, { Component } from 'react';
-import './App.css';
+import firebase from 'firebase/app';
+import classnames from 'classnames';
+import formatDate from 'date-fns/format';
+
+let userPromise;
 
 class App extends Component {
 	state = {
-		messageState: ''
+		entryText: '',
+		submitting: false
 	};
-	submit = () => {
-		const { messageState } = this.state;
-		console.log('This will submit', `'${messageState}'`);
-		this.setState({ messageState: '' });
+	componentDidMount() {
+		userPromise = new Promise((resolve, reject) => {
+			firebase.auth().onAuthStateChanged(user => {
+				if (user) {
+					resolve(user);
+				} else {
+					firebase
+						.auth()
+						.signInAnonymously()
+						.catch(err => {
+							console.error(err);
+							reject(err);
+						});
+				}
+			});
+		});
+	}
+	submit = async () => {
+		this.setState({ submitting: true });
+		const { uid } = await userPromise;
+		const { entryText } = this.state;
+
+		const entry = {
+			body: entryText,
+			timestamp: new Date()
+		};
+
+		await firebase
+			.firestore()
+			.collection('users')
+			.doc(uid)
+			.collection('entries')
+			.add(entry)
+			.catch(err => {
+				console.error(err);
+				return;
+			});
+
+		this.setState({ entryText: '', submitting: false });
 	};
 	handleChange = ({ target: { value } }) => {
-		this.setState({ messageState: value });
+		this.setState({ entryText: value });
 	};
 	render() {
-		const { messageState } = this.state;
+		const { entryText, submitting } = this.state;
+
+		const submitButtonClasses = classnames(
+			'button is-large is-fullwidth is-primary',
+			{
+				'is-loading': submitting
+			}
+		);
+
+		const dateString = formatDate(new Date(), 'MMM D, YYYY');
+
 		return (
-			<div className="App">
-				<h1>What did you work on today?</h1>
-				<div>
-					<textarea value={messageState} onChange={this.handleChange} />
-				</div>
-				<div>
-					<button onClick={this.submit}>Submit</button>
+			<div className="App section">
+				<div className="container">
+					<div className="columns">
+						<div className="column">
+							<h1 className="title">
+								{dateString} - What did you work on today?
+							</h1>
+							<div className="field">
+								<div className="control">
+									<textarea
+										className="textarea"
+										value={entryText}
+										onChange={this.handleChange}
+									/>
+								</div>
+							</div>
+							<div className="field">
+								<div className="control">
+									<button
+										className={submitButtonClasses}
+										disabled={submitting}
+										onClick={this.submit}
+									>
+										Submit
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'bulma/css/bulma.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const config = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
